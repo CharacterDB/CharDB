1cb3ff088ecfc851cfb9737a3f08db1620cafd0f
b3ny4 <b3ny4@protonmail.com>
2023-03-06 09:50:12 +0100

    fixed api

--- routers/api/characters.js
+++ routers/api/characters.js
@@ -17,10 +17,13 @@ var connection = await mysql.createConnection({
 // bind routes
 router.get('/', async (req, res) => {
     const { pronunciation = '' } = req.query;
-    let [rows, _] = await connection.query('SELECT id, symbol, pinyin, meaning, keyword FROM transcription LEFT JOIN meaning ON transcription.id = meaning.character_id WHERE pinyin LIKE ?', `%${pronunciation}%`);
-    const data = rows.reduce((acc, { id, symbol, pinyin, meaning, keyword }) => {
-        acc[id] = acc[id] || { id, symbol, pinyin, meanings: [] };
-        acc[id].meanings.push(meaning);
+    const limit = 100;
+    let [[rows, __], _] = await connection.query('CALL get_characters(?, ?)', [`%${pronunciation}%`, limit]);
+    const data = rows.reduce((acc, { id, symbol, transcription, meaning, keyword, primitive }) => {
+        acc[id] = acc[id] || { id, symbol, transcription, keyword: null, meanings: [], primitives: [] };
+        if (keyword && keyword.equals(Buffer.from([1])) && meaning) acc[id].keyword = meaning;
+        else if (meaning) acc[id].meanings.push(meaning);
+        if (primitive) acc[id].primitives.push(primitive);
         return acc;
     }, {});
     res.send(data);
@@ -28,23 +31,37 @@ router.get('/', async (req, res) => {
 
 router.get('/:id(\\d+)/', async (req, res) => {
     const { id } = req.params;
-    let [rows, _] = await connection.query('SELECT id, symbol, pinyin, meaning, keyword FROM transcription LEFT JOIN meaning ON transcription.id = meaning.character_id WHERE character_id=?', id);
-    const data = rows.reduce((acc, { id, symbol, pinyin, meaning, keyword }) => {
-        acc = acc || { id, symbol, pinyin, meanings: [] };
-        acc.meanings.push(meaning);
-        return acc;
-    }, null);
+    let [rows, _] = await connection.query('SELECT id, symbol, transcription FROM transcription WHERE id=?', id);
+    const data = rows[0];
+    [rows, _] = await connection.query('SELECT meaning, keyword FROM meaning WHERE character_id = ?', id);
+    data.meanings = [];
+    for (let row of rows) {
+        if (row.keyword.equals(Buffer.from([1]))) data.keyword = row.meaning;
+        else data.meanings.push(row.meaning);
+    }
+    [rows, _] = await connection.query('SELECT primitive_id AS primitive FROM composition WHERE character_id = ?', id);
+    data.primitives = [];
+    for (let row of rows) {
+        data.primitives.push(row.primitive);
+    }
     res.send(data);
 });
 
 router.get('/:symbol/', async (req, res) => {
     const { symbol } = req.params;
-    let [rows, _] = await connection.query('SELECT id, symbol, pinyin, meaning, keyword FROM transcription LEFT JOIN meaning ON transcription.id = meaning.character_id WHERE symbol=?', symbol);
-    const data = rows.reduce((acc, { id, symbol, pinyin, meaning, keyword }) => {
-        acc = acc || { id, symbol, pinyin, meanings: [] };
-        acc.meanings.push(meaning);
-        return acc;
-    }, null);
+    let [rows, _] = await connection.query('SELECT id, symbol, transcription FROM transcription WHERE symbol = ?', symbol);
+    const data = rows[0];
+    [rows, _] = await connection.query('SELECT meaning, keyword FROM meaning WHERE character_id = ?', data.id);
+    data.meanings = [];
+    for (let row of rows) {
+        if (row.keyword.equals(Buffer.from([1]))) data.keyword = row.meaning;
+        else data.meanings.push(row.meaning);
+    }
+    [rows, _] = await connection.query('SELECT primitive_id AS primitive FROM composition WHERE character_id = ?', data.id);
+    data.primitives = [];
+    for (let row of rows) {
+        data.primitives.push(row.primitive);
+    }
     res.send(data);
 })
 
